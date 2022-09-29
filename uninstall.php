9bbe5faad91cefcb3898d64c2f3b160bf3bb0770
Eric Moss <eric@villagemedia.ca>
2022-09-29 12:04:24 -0400

    Use `die;` instead of `exit ();` based on WP example

--- uninstall.php
+++ uninstall.php
@@ -1,11 +1,11 @@
 <?php
 
 //if uninstall not called from WordPress exit
-if ( !defined( 'WP_UNINSTALL_PLUGIN' ) )
-	exit ();
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	die;
+}
 
 // Remove the 'menu_order' column
 global $wpdb;
 $sql = "ALTER TABLE `{$wpdb->terms}` DROP COLUMN `menu_order`;";
 $wpdb->query( $sql );
-	
